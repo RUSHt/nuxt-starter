c86cd3efa639306a6fec45a9a9b65c6891b94f48
RUSHt <petera@rush.technology>
2021-10-19 15:20:17 +0100

    .

--- server/api/test.js
+++ server/api/test.js
@@ -1,5 +1,8 @@
 export default ( req, res ) => {
     console.log('test.js req.url',req.url);
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 200;
+    return res.end('Hello World');
     return {
         statusCode: 302,
         headers: {

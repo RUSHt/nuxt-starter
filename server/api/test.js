77485b35248c9fbd264dd15988b95157b059e3dd
RUSHt <petera@rush.technology>
2021-10-19 15:16:54 +0100

    added return value lambda

--- server/api/test.js
+++ server/api/test.js
@@ -1,4 +1,10 @@
 export default ( req, res ) => {
     console.log('test.js req.url',req.url);
-    res.end('Hello World');
+    return {
+        statusCode: 302,
+        headers: {
+            'Cache-Control': 'no-cache',
+        },
+        body: JSON.stringify({ msg: `you're good` }),
+    }
 }

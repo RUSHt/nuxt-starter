b840e5d451cffbd6baaa6ca3f9204a3f6eea025e
RUSHt <petera@rush.technology>
2021-10-19 15:23:27 +0100

    Return Object from test.js

--- server/api/test.js
+++ server/api/test.js
@@ -1,13 +1,10 @@
 export default ( req, res ) => {
     console.log('test.js req.url',req.url);
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    return res.end('Hello World');
     return {
         statusCode: 302,
         headers: {
             'Cache-Control': 'no-cache',
         },
-        body: JSON.stringify({ msg: `you're good` }),
+        body: { msg: `you're good` },
     }
 }

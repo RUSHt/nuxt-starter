578c6603c0f6fe8b888afb508227cf6e2bbf58be
RUSHt <petera@rush.technology>
2021-10-19 15:23:52 +0100

    .

--- server/api/test.js
+++ server/api/test.js
@@ -1,10 +1,4 @@
 export default ( req, res ) => {
     console.log('test.js req.url',req.url);
-    return {
-        statusCode: 302,
-        headers: {
-            'Cache-Control': 'no-cache',
-        },
-        body: { msg: `you're good` },
-    }
+    return 'Hello World'
 }
